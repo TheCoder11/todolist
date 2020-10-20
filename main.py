2615effad0c097b9830c2aa03efdfb40922eb717
TheCoder11 <jtsjohn05@gmail.com>
2020-10-20 11:39:16 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,97 @@
+def on_up_pressed():
+    global selected_number
+    if len(currentList2) > 0:
+        if selected_number == 0:
+            selected_number = len(currentList2) - 1
+        else:
+            selected_number = selected_number - 1
+    print(generateNewListString(currentList2, selected_number))
+controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
+
+def on_b_pressed():
+    global tmp_newTask
+    if controller.right.is_pressed():
+        print("SUGGESTED TASKS, Press B to select")
+        currentList2 = suggestedTasks
+        print(generateNewListString(suggestedTasks, selected_number))
+        
+        def on_a_pressed_suggested():
+            if suggestedTasks[selected_number] in currentTasks:
+                print("Suggestion has already been added")
+            else:
+                currentTasks.append(suggestedTasks[selected_number])
+                print("SUGGESTION ADDED")
+            currentList2 = currentTasks
+            controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+        controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed_suggested)
+    
+    elif controller.left.is_pressed():
+        print("DELETED TASKS: \n")
+        
+        print(generateNewListString(deletedTasks, 0))
+        
+    else:
+        tmp_newTask = game.ask_for_string("Please enter new task")
+        if tmp_newTask in currentTasks:
+            print("Task already present!")
+        else:
+            currentTasks.append(tmp_newTask)
+            if deletedTasks.index(tmp_newTask) >= 0:
+                suggestedTasks.append(tmp_newTask)
+                deletedTasks.remove_at(deletedTasks.index(tmp_newTask))
+                deletionTimes.remove_at(deletedTasks.index(tmp_newTask))
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
+def on_down_pressed():
+    global selected_number
+    if len(currentList2) > 0:
+        if selected_number == len(currentList2) - 1:
+            selected_number = 0
+        else:
+            selected_number = selected_number + 1
+    print(generateNewListString(currentList2, selected_number))
+controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
+
+def on_a_pressed():
+    global temp_deletedVariable
+    temp_deletedVariable = currentTasks.remove_at(selected_number)
+    deletedTasks.append(temp_deletedVariable)
+    suggestedTasks.append(temp_deletedVariable)
+    deletionTimes.append(game.runtime())
+    print("" + temp_deletedVariable + " deleted")
+
+deletionTimes: List[number] = []
+temp_deletedVariable = ""
+tmp_newTask = ""
+currentList2: List[str] = []
+deletedTasks: List[str] = []
+selected_number = 0
+currentTasks: List[str] = []
+suggestedTasks: List[str] = []
+temp_deletedVariable2 = 0
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+selected_number = 0
+currentTasks = []
+suggestedTasks = []
+deletedTasks = []
+currentList2 = currentTasks
+game.splash("Please look at the console")
+
+def generateNewListString(usedList: List[str], selectedNumber: number = 0):
+    generatedString = " --------------------- \n"
+    for item in usedList:
+        if usedList.index(item) == selectedNumber:
+            generatedString = generatedString + "(X). " + item
+        else:
+            generatedString = generatedString + "(O). " + item
+        generatedString = generatedString + "\n"
+    generatedString = generatedString + " ---------------------- \n"
+    return generatedString
+
+def on_on_update():
+    for value in suggestedTasks:
+        if deletedTasks.index(value) >= 0:
+            if deletionTimes[deletedTasks.index(value)] < game.runtime() - 20000:
+                suggestedTasks.remove_at(suggestedTasks.index(value))
+                print("" + value + " deleted due to time")
+game.on_update(on_on_update)
---
+++ main.ts
@@ -0,0 +1,118 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function on_up_pressed() {
+    
+    if (currentList2.length > 0) {
+        if (selected_number == 0) {
+            selected_number = currentList2.length - 1
+        } else {
+            selected_number = selected_number - 1
+        }
+        
+    }
+    
+    console.log(generateNewListString(currentList2, selected_number))
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+    let currentList2: string[];
+    
+    if (controller.right.isPressed()) {
+        console.log("SUGGESTED TASKS, Press B to select")
+        currentList2 = suggestedTasks
+        console.log(generateNewListString(suggestedTasks, selected_number))
+        controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed_suggested() {
+            if (currentTasks.indexOf(suggestedTasks[selected_number]) >= 0) {
+                console.log("Suggestion has already been added")
+            } else {
+                currentTasks.push(suggestedTasks[selected_number])
+                console.log("SUGGESTION ADDED")
+            }
+            
+            let currentList2 = currentTasks
+            controller.A.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
+        })
+    } else if (controller.left.isPressed()) {
+        console.log("DELETED TASKS: \n")
+        console.log(generateNewListString(deletedTasks, 0))
+    } else {
+        tmp_newTask = game.askForString("Please enter new task")
+        if (currentTasks.indexOf(tmp_newTask) >= 0) {
+            console.log("Task already present!")
+        } else {
+            currentTasks.push(tmp_newTask)
+            if (deletedTasks.indexOf(tmp_newTask) >= 0) {
+                suggestedTasks.push(tmp_newTask)
+                deletedTasks.removeAt(deletedTasks.indexOf(tmp_newTask))
+                deletionTimes.removeAt(deletedTasks.indexOf(tmp_newTask))
+            }
+            
+        }
+        
+    }
+    
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function on_down_pressed() {
+    
+    if (currentList2.length > 0) {
+        if (selected_number == currentList2.length - 1) {
+            selected_number = 0
+        } else {
+            selected_number = selected_number + 1
+        }
+        
+    }
+    
+    console.log(generateNewListString(currentList2, selected_number))
+})
+function on_a_pressed() {
+    
+    temp_deletedVariable = currentTasks.removeAt(selected_number)
+    deletedTasks.push(temp_deletedVariable)
+    suggestedTasks.push(temp_deletedVariable)
+    deletionTimes.push(game.runtime())
+    console.log("" + temp_deletedVariable + " deleted")
+}
+
+let deletionTimes : number[] = []
+let temp_deletedVariable = ""
+let tmp_newTask = ""
+let currentList2 : string[] = []
+let deletedTasks : string[] = []
+let selected_number = 0
+let currentTasks : string[] = []
+let suggestedTasks : string[] = []
+let temp_deletedVariable2 = 0
+controller.A.onEvent(ControllerButtonEvent.Pressed, on_a_pressed)
+selected_number = 0
+currentTasks = []
+suggestedTasks = []
+deletedTasks = []
+currentList2 = currentTasks
+game.splash("Please look at the console")
+function generateNewListString(usedList: string[], selectedNumber: number = 0): string {
+    let generatedString = ` --------------------- 
+`
+    for (let item of usedList) {
+        if (usedList.indexOf(item) == selectedNumber) {
+            generatedString = generatedString + "(X). " + item
+        } else {
+            generatedString = generatedString + "(O). " + item
+        }
+        
+        generatedString = generatedString + "\n"
+    }
+    generatedString = generatedString + ` ---------------------- 
+`
+    return generatedString
+}
+
+game.onUpdate(function on_on_update() {
+    for (let value of suggestedTasks) {
+        if (deletedTasks.indexOf(value) >= 0) {
+            if (deletionTimes[deletedTasks.indexOf(value)] < game.runtime() - 20000) {
+                suggestedTasks.removeAt(suggestedTasks.indexOf(value))
+                console.log("" + value + " deleted due to time")
+            }
+            
+        }
+        
+    }
+})
